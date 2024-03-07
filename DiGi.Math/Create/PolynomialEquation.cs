7df08ff9783d414f353740e446bf673451efe97d
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2024-03-07 18:19:19 +0100

    Create Method for PolynomialEquation Implemented

---
+++ DiGi.Math/Create/PolynomialEquation.cs
@@ -0,0 +1,38 @@
+﻿using DiGi.Math.Classes;
+using MathNet.Numerics;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DiGi.Math
+{
+    public static partial class Create
+    {
+        public static PolynomialEquation PolynomialEquation(IEnumerable<double> x, IEnumerable<double> y, int order = -1)
+        {
+            if (x == null || y == null)
+            {
+                return null;
+            }
+
+            int count = x.Count();
+            if (count == 0 || y.Count() != count)
+            {
+                return null;
+            }
+
+            if (x.Count() == 1)
+            {
+                return null;
+            }
+
+            int order_Temp = order;
+            if (order_Temp == -1)
+            {
+                order_Temp = count - 1;
+            }
+
+            Polynomial polynomial = Polynomial.Fit(x.ToArray(), y.ToArray(), order_Temp, MathNet.Numerics.LinearRegression.DirectRegressionMethod.NormalEquations);
+            return polynomial?.ToDiGi();
+        }
+    }
+}
